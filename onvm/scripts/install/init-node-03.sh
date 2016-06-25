775219ec6903f9a407414138631bb35ffe22ae35
Tong Li <litong01@us.ibm.com>
2016-06-25 18:34:02 -0400

    OVN based neutron can also use the security rules

--- onvm/scripts/install/init-node-03.sh
+++ onvm/scripts/install/init-node-03.sh
@@ -5,19 +5,14 @@
 # $4 public net end_ip
 # $5 public net gateway
 
-source /onvm/scripts/ini-config
-eval $(parse_yaml '/onvm/conf/nodes.conf.yml' 'leap_')
+echo "Setting security rules for demo project..."
 
-if [ "$leap_network" != 'ovn' ]; then
-  echo "Setting security rules for demo project..."
+source ~/demo-openrc.sh
 
-  source ~/demo-openrc.sh
+neutron security-group-rule-create --direction ingress --protocol icmp --remote-ip-prefix 0.0.0.0/0 default
+neutron security-group-rule-create --direction egress --protocol icmp --remote-ip-prefix 0.0.0.0/0 default
 
-  neutron security-group-rule-create --direction ingress --protocol icmp --remote-ip-prefix 0.0.0.0/0 default
-  neutron security-group-rule-create --direction egress --protocol icmp --remote-ip-prefix 0.0.0.0/0 default
-
-  neutron security-group-rule-create --direction ingress --protocol tcp \
-    --port-range-min 22 --port-range-max 22 --remote-ip-prefix 0.0.0.0/0 default
-fi
+neutron security-group-rule-create --direction ingress --protocol tcp \
+  --port-range-min 22 --port-range-max 22 --remote-ip-prefix 0.0.0.0/0 default
 
 echo "Init-node-03 is now complete!"
