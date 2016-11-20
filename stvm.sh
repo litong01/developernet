d59226eb52b2bffb6b73c7a22bfcb3adb46fbe42
Tong Li <litong01@us.ibm.com>
2016-11-19 22:54:07 -0500

    added one extra compute node

--- stvm.sh
+++ stvm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-machines=('os-controller' 'os-compute01' 'os-compute02')
+machines=('os-controller' 'os-compute01' 'os-compute02' 'os-compute03')
 
 for key in ${machines[@]}; do
     echo "Shutting down $key"
